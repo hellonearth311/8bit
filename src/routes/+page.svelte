245156f4e9ac01707710c8e52ea992638479db86
swaritnarang <naranswarit02@gmail.com>
2026-03-13 21:04:47 -0500

    added basic startup animation that autoplays, going to customize it later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <!-- eslint-disable svelte/no-navigation-without-resolve -->
 
 <div id="loadingSkip">
-    <p>loading... // <a href="/" id="skipIntro">skip intro</a></p>
+    <video src="/assets/vid/gamecube-startup.mp4" id="introVideo" autoplay muted playsinline><track kind=captions></video>
+    
+    <p><a href="/" id="introText">skip intro</a></p>
 </div>
 
